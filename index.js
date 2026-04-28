e37dfde2e0767d1c78e7971f357f623ca6d9d1b7
ZizaKariuki <zizawabetti@gmail.com>
2026-04-28 18:08:34 +0300

    DOM learning

--- index.js
+++ index.js
@@ -2,46 +2,74 @@ const bookStore = {
     name: 'Flatbooks Technical Books',
     books: [
         {
-            id:1,
+            id: 1,
             title: 'Eloquent JavaScript: A Modern Introduction to Programming',
             author: 'Marjin Haverbeke',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-            
+
         },
         {
-            id:2,
+            id: 2,
             title: 'JavaScript & JQuery: Interactive Front-End Web Development',
             author: 'Jon Duckett',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
         },
         {
-            id:3,
+            id: 3,
             title: 'JavaScript: The Good Parts',
             author: 'Douglas Crockford',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
         },
         {
-            id:4,
+            id: 4,
             title: 'JavaScript: The Definitive Guide',
             author: 'David Flanagan',
             imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-            
+
         },
         {
-            id:5,
+            id: 5,
             title: 'You Don’t Know JS',
             author: 'Kyle Simpson',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
         },
         {
-            id:6,
+            id: 6,
             title: 'Cracking the Coding Interview',
             author: 'Gayle Laakmann McDowell',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-            
+
         }
     ]
 }
 
 // Write your code here!
+//Manipulating existing elements
+let bookStoreTitle = document.querySelector("#header");
+bookStoreTitle.textContent = bookStore.name;
 
+//Book elements:
+
+bookStore.books.forEach((book) => {
+    //Create elements for each book:
+    let bookContainer = document.createElement("li");
+    let bookTitle = document.createElement("h3");
+    let bookAuthor = document.createElement("p");
+    let bookImage = document.createElement("img");
+
+    //Change the textContent of each element to match book object:
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+
+    //Append book elements to bookContainer:
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+
+    //Append bookContainer element to bookList:
+    let bookList = document.getElementById("book-list");
+    bookList.appendChild(bookContainer);
+});
